d419bc25a018339ed711fd24a6116649cd6cb596
trantuvan <trantuvan.kan@gmail.com>
2022-12-20 16:49:22 +0700

    fix entry point by using path from commonjs

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "src/index.js",
+  entry: path.resolve(__dirname, "src/index.js"),
   output: {
     path: path.join(__dirname, "dist"),
     filename: "index_bundle.js",
