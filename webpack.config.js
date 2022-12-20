540364b649d603ab90c96fa236604f8e17b81397
trantuvan <trantuvan.kan@gmail.com>
2022-12-20 16:25:54 +0700

    use babel-loader to read js file

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,15 @@ module.exports = {
     path: path.join(__dirname, "dist"),
     filename: "index_bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
 };
